88eb1412ab55dab60e5c2c446419575cc9656269
ziieng <cjengelhardt@gmail.com>
2021-02-05 14:57:40 -0800

    remove old items that don't apply to this project

--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -4,67 +4,34 @@ import CardContainer from "react-bootstrap/CardContainer";
 import Row from "react-bootstrap/Row";
 
 function Gallery() {
-  const [user, setUser] = useState({});
+  const [sort, setSort] = useState("");
   const [users, setUsers] = useState([]);
-  const [userIndex, setUserIndex] = useState(0);
 
   // When the component mounts, a call will be made to get random users.
   useEffect(() => {
     loadUsers();
   }, []);
 
-  function nextUser(userIndex) {
-    // Ensure that the user index stays within our range of users
-    if (userIndex >= users.length) {
-      userIndex = 0;
-    }
-    setUser(users[userIndex]);
-    setUserIndex(userIndex);
-  }
-
-  function previousUser(userIndex) {
-    // Ensure that the user index stays within our range of users
-    if (userIndex < 0) {
-      userIndex = users.length - 1;
-    }
-    setUser(users[userIndex]);
-    setUserIndex(userIndex);
-  }
-
   function handleBtnClick(event) {
     // Get the title of the clicked button
     const btnName = event.target.getAttribute("data-value");
     if (btnName === "next") {
-      const newUserIndex = userIndex + 1;
-      nextUser(newUserIndex);
-    } else {
-      const newUserIndex = userIndex - 1;
-      previousUser(newUserIndex);
     }
   }
 
   function loadUsers() {
-    API.getLanguagesList()
-      .then(languages => {
-        API.getUsersByLanguage(languages[0]).then(users => {
+    API.getUsers().then(users => {
           setUsers(users);
-          setUser(users[0]);
-        });
-      })
+      setSort("Name Asc");
+        })
       .catch(err => console.log(err));
   }
 
   return (
     <div>
-      <h1 className="text-center">Welcome to LinkedUp</h1>
-      <h3 className="text-center">Click on the arrows to browse users</h3>
+      <h1 className="text-center">Company Directory</h1>
+      <h3 className="text-center">Click on the column headings to change sort</h3>
       <Row>
-        <CardContainer
-          title={user.login}
-          image={user.image}
-          language={user.language}
-          handleBtnClick={handleBtnClick}
-        />
       </Row>
     </div>
   );
