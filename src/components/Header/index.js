3b8b98ebfb9362d7655e6c1a97eadc31473e9c67
ziieng <cjengelhardt@gmail.com>
2021-02-05 23:27:14 -0800

    change styling for color scheme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Header(props) {
-  return <div className="jumbotron-fluid header mb-3 mx-0">
+  return <div className="jumbotron-fluid header bg-info mb-3">
     <h1 className="text-center">Company Directory</h1>
     <h3 className="text-center">Click on the column headings to change sort</h3>
   </div>
